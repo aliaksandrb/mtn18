c3bee55048b1bd53ea6ced2f92b599e5892d58c2
AlehYarmalovich <35954548+AlehYarmalovich@users.noreply.github.com>
2018-04-25 17:10:12 +0300

    Update aleh_yarmalovich_l1.rb

--- homeworks/aleh_yarmalovich_l1.rb
+++ homeworks/aleh_yarmalovich_l1.rb
@@ -13,7 +13,7 @@ def task_2(input)
   input.each_line do |line|
     unless line.downcase.include? 'error'
       pars = line.match %r/(\b(\d{1,3}\.){3}\d{1,3}).*(\[.*\]).*( \/.[^\s]*)/
-      res << "#{pars[3][1...-1]} FROM: #{pars[1]} TO:#{pars[4].to_s.upcase} \n"
+      res << "#{pars[3][1...-1]} FROM: #{pars[1]} TO:#{pars[4].to_s.upcase}"
     end
   end
   res
