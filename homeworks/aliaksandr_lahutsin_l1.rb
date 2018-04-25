cf9732843e1cb5b60a6029bd28be7e9e26f54d75
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-04-25 17:14:18 +0300

    Update aliaksandr_lahutsin_l1.rb

--- homeworks/aliaksandr_lahutsin_l1.rb
+++ homeworks/aliaksandr_lahutsin_l1.rb
@@ -47,6 +47,7 @@ FOR EXAMPLE "23/Apr/2018:20:30:39 +0300 FROM: 10.6.246.103 TO: /GRID/2/MESSAGES"
 
 =end
 
+
 #---------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------
 
@@ -165,20 +166,20 @@ end
 
 # check task3
 
-#def task3(text)
-#    parse_string(text)
-#end
-
+def task_3(text)
+    parse_string(text)
+end
 
 =begin
 
-puts task1('''10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+puts task_1('''10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
 10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
 2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
 10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
 ''')
 
-puts task2('''2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+
+puts task_2('''2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
 2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
 2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
 2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
@@ -187,6 +188,6 @@ puts task2('''2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core wi
 2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
 ''')
 
-puts task3("344353445h4.8ty83h2v72yi3g2t35y457%^&*()_k4wuaehzw64jkie57lk7kwaa84t8.2pgib93uh")
+puts task_3("344353445h4.8ty83h2v72yi3g2t35y457%^&*()_k4wuaehzw64jkie57lk7kwaa84t8.2pgib93uh")
 
 =end
