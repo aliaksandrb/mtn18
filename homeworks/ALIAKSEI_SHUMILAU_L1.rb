9bea62919b1aebc26fe280fe0d16e0bc6f222b58
Alexey <shumilovy@mail.ru>
2018-04-25 17:02:59 +0300

    Update ALIAKSEI_SHUMILAU_L1.rb

--- homeworks/ALIAKSEI_SHUMILAU_L1.rb
+++ homeworks/ALIAKSEI_SHUMILAU_L1.rb
@@ -1,6 +1,4 @@
-
 #TASK1
-
 def task1(input)
 string.each_line do |line|
   line if (line['error'])
@@ -9,11 +7,8 @@ line
 end
 
 #TASK2
-
 def task2()
 string.each_line do |line|
   string.split('[')[1].split(']')[0] + " FROM:" + string.split[0] + " TO:" + string.upcase.split('POST')[1].split('HTTP')[0]
 end
-
 end
-:
