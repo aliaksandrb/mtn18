a11c930ba74acec32bc8bc96f420d4740428779b
AlehYarmalovich <35954548+AlehYarmalovich@users.noreply.github.com>
2018-04-25 17:46:16 +0300

    Update aleh_yarmalovich_l1.rb

--- homeworks/aleh_yarmalovich_l1.rb
+++ homeworks/aleh_yarmalovich_l1.rb
@@ -10,10 +10,13 @@ end
 
 def task_2(input)
   res = []
+  reg = %r/(\b(\d{1,3}\.){3}\d{1,3}).*(\[.*\]).*( \/.[^\s]*)/
   input.each_line do |line|
-    pars = line.match %r/(\b(\d{1,3}\.){3}\d{1,3}).*(\[.*\]).*( \/.[^\s]*)/
-    unless line.downcase.include? 'error'
-      res << "#{pars[3][1...-1]} FROM: #{pars[1]} TO:#{pars[4].to_s.upcase}"
+    if line.match reg
+      unless line.downcase.include? 'error'
+        pars = line.match reg
+        res << "#{pars[3][1...-1]} FROM: #{pars[1]} TO:#{pars[4].to_s.upcase}"
+      end
     end
   end
   res
