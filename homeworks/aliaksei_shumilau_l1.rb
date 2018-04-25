5c4f14dd20ec06b12d6a4ec33b0d8644c5dd5006
Alexey <shumilovy@mail.ru>
2018-04-25 18:04:14 +0300

    Update aliaksei_shumilau_l1.rb

--- homeworks/aliaksei_shumilau_l1.rb
+++ homeworks/aliaksei_shumilau_l1.rb
@@ -1,14 +1,18 @@
 #TASK1
 def task1(input)
+  task1 = []
   input.each_line do |line|
-    line if (line.downcase['error'])
+    if (line.downcase['error'])
+      task1 << line[]
   end
-  line
+  task1[]
 end
 
 #TASK2
-def task2()
-  string.each_line do |line|
-    string.split('[')[1].split(']')[0] + " FROM:" + string.split[0] + " TO:" + string.upcase.split('POST')[1].split('HTTP')[0]
+def task2(input)
+  task2 = []
+  input.each_line do |line|
+    task2 << line.split('[')[1].split(']')[0] + " FROM:" + line.split[0] + " TO:" + line.upcase.split('POST')[1].split('HTTP')[0] if (line.downcase.match("^(?!.*error).*$"))
   end
-  end
+  task2
+end
