d6d6f2ee74f2eb74169731f05d9d47852db08e2f
AlehYarmalovich <35954548+AlehYarmalovich@users.noreply.github.com>
2018-04-25 17:04:48 +0300

    Update aleh_yarmalovich_l1.rb

--- homeworks/aleh_yarmalovich_l1.rb
+++ homeworks/aleh_yarmalovich_l1.rb
@@ -9,12 +9,30 @@ def task_1(input)
 end
 
 def task_2(input)
-  res = [nil]
+  res = []
   input.each_line do |line|
     unless line.downcase.include? 'error'
       pars = line.match %r/(\b(\d{1,3}\.){3}\d{1,3}).*(\[.*\]).*( \/.[^\s]*)/
-      res << "#{pars[3][1...-1]} FROM: #{pars[1]} TO:#{pars[4].to_s.upcase}"
+      res << "#{pars[3][1...-1]} FROM: #{pars[1]} TO:#{pars[4].to_s.upcase} \n"
     end
   end
+  res
+end
+
+def task_3(input)
+  res = 0
+  input.each_line do |line|
+    require 'time'
+    if line.downcase.include? 'calling core with action:'
+      datetime = Time.parse((line.match /.+?(?=\s\D)/).to_s)
+      res = datetime - res
+    end
+  end
+  res
+end
+
+def task_4(input)
+  res = 0
+  input.each_char { |sum| res += sum.to_i }
   res
 end
