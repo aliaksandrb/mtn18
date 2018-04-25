2933af9f14eb3277981dfec9a07c339a79a9d2b2
Alexey <shumilovy@mail.ru>
2018-04-25 18:34:47 +0300

    Update

--- homeworks/aliaksei_shumilau_l1.rb
+++ homeworks/aliaksei_shumilau_l1.rb
@@ -1,18 +1,42 @@
+#######################################################
 #TASK1
-def task1(input)
-  task1 = []
+def task_1(input)
+  list = ''
   input.each_line do |line|
-    if (line.downcase['error'])
-      task1 << line[]
+     if (line.downcase.include? 'error')
+       list << line[0..-2]
+     end
   end
-  task1[]
+  list
+end
+######################################################
+#TASK2
+def task_2(input)
+  list = []
+  input.each_line do |line|
+     list.push(line.split('[')[1].split(']')[0] + ' FROM: ' + line.split[0] + ' TO:' + line.upcase.split('POST')[1].split('HTTP')[0][0..-1]) if (line.downcase.match("^(?!.*error).*$"))
+  end
+  list
 end
 
-#TASK2
-def task2(input)
-  task2 = []
+######################################################
+#TASK3
+def task_3(input)
+  require 'time'
+  list = []
   input.each_line do |line|
-    task2 << line.split('[')[1].split(']')[0] + " FROM:" + line.split[0] + " TO:" + line.upcase.split('POST')[1].split('HTTP')[0] if (line.downcase.match("^(?!.*error).*$"))
+    list.push Time.parse(line) if line.include? 'Calling'
   end
-  task2
+    list[1] - list[0]
+end
+
+  
+#######################################################  
+#TASK4
+def task_4(input)
+  sum = 0
+  input.each_char do |letter|
+    sum += letter.to_i if letter.to_i.is_a? Integer
+  end
+  sum
 end
