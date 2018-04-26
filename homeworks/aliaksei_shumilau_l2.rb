abd975bc32dc9ccfe47f548190b1c10d89773f91
Alexey <shumilovy@mail.ru>
2018-04-26 17:19:04 +0300

    Update aliaksei_shumilau_l2.rb

--- homeworks/aliaksei_shumilau_l2.rb
+++ homeworks/aliaksei_shumilau_l2.rb
@@ -4,28 +4,13 @@ def task_2_1(n)
   if n == 1
     puts 0
   else
-    task_2_1 = [0, 1]
+    fib = [0, 1]
     (n - 2).times do
-      task_2_1 << task_2_1[-1] + task_2_1[-2]
+      fib << fib[-1] + fib[-2]
     end
-    puts task_2_1.join(' ')
+    puts fib.join(' ')
   end
 end
-
-######################################################
-# Task_2.2 Yaml_parcing
-
-
-
-
-######################################################
-# Task_2.3 Returning an array
-
-
-
-
-
-
 
 ######################################################
 # Task_2.4 The row palindrome
