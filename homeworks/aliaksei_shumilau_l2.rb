c85a03ac3ac582902729cda14378f6111999423f
Alexey <shumilovy@mail.ru>
2018-04-26 15:52:49 +0300

    update

--- homeworks/aliaksei_shumilau_l2.rb
+++ homeworks/aliaksei_shumilau_l2.rb
@@ -1,30 +1,30 @@
 #######################################################
-#Task_2.1 Fibonacci_numbers
+# Task_2.1 Fibonacci_numbers
 
-def fibo(n) 
-  a=0 
-  b=1 
-  for i in 0..n 
-    c=a+b 
+def fibo(n)
+  a = 0
+  b = 1
+  for i in 0..n
+    c=a+b
     print "#{c} "
-    a=b 
-    b=c 
+    a=b
+    b=c
 end
 end
 
 ######################################################
-#Task_2.2 Yaml_parcing
+# Task_2.2 Yaml_parcing
 
 
 
 
 
 ######################################################
-#Task_2.3 Return an array
+# Task_2.3 Return an array
 
   
 
 
 #######################################################  
-#Task_2.4 Palindrome of the string
+# Task_2.4 Palindrome of the string
 
