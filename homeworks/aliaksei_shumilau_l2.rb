520ff5faaff530ec81237486d153bec294ef4f29
Alexey <shumilovy@mail.ru>
2018-04-26 17:58:17 +0300

    Update aliaksei_shumilau_l2.rb

--- homeworks/aliaksei_shumilau_l2.rb
+++ homeworks/aliaksei_shumilau_l2.rb
@@ -13,8 +13,14 @@ def task_2_1(n)
 end
 
 ######################################################
+# Task_2.3 Returning an array
+def task_2_3(array)
+  array.flatten.uniq.sort.reverse
+end
+
+######################################################
 # Task_2.4 The row palindrome
-def task_2_4(string)
-  return true if string == string.reverse
-  false
+def task2_4(string)
+  string = string.downcase.scan(/\w/)
+  string == string.reverse
 end
