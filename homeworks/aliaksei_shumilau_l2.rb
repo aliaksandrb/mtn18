273f5ea174247a53e8dec453907e046ee1fc5baa
Alexey <shumilovy@mail.ru>
2018-04-26 20:22:59 +0300

    Update aliaksei_shumilau_l2.rb

--- homeworks/aliaksei_shumilau_l2.rb
+++ homeworks/aliaksei_shumilau_l2.rb
@@ -1,15 +1,15 @@
 #######################################################
 # Task_2.1 Fibonacci_numbers
 def task_2_1(n)
-  if n == 0
-    puts 0
-  else
-    fib = [1, 1]
-    (n - 2).times do
-      fib << fib[-1] + fib[-2]
-    end
-    puts fib.join(' ')
+  return [n] if (0..1).cover? n
+  res = []
+  x = 1
+  z = 1
+  n.times do
+    res << z
+    x, z = z, x + z
   end
+  res
 end
 
 ######################################################
