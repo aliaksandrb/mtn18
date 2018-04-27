dd06685a75a3383f98bb776e09f8287dd742b75d
Alexey <shumilovy@mail.ru>
2018-04-27 09:35:31 +0300

    update

--- homeworks/aliaksei_shumilau_l2.rb
+++ homeworks/aliaksei_shumilau_l2.rb
@@ -13,6 +13,27 @@ def task_2_1(n)
 end
 
 ######################################################
+# Task_2.3 Yaml_parcing
+def task_2_2(input)
+  require 'yaml'
+  output = []
+  return output if input == ''
+  YAML.safe_load(input).each do |key, value|
+    data = {}
+    value.each do
+      data[:db] = value['database']
+      data[:user] = value['username'] if value.include?('username')
+      data[:password] = value['password'] if value.include?('password')
+      my_pool = value.key?('pool') ? value['pool'] : 1
+      my_timeout = value.key?('timeout') ? value['timeout'] : 1000
+      data[:magic_number] = (my_pool * my_timeout)
+    end
+    output << Hash[key.to_sym, data]
+  end
+  output
+end
+
+######################################################
 # Task_2.3 Returning an array
 def task_2_3(array)
   array.flatten.uniq.sort.reverse
