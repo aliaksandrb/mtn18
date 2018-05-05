49e2c376c5339420af4ca0343044ecdb5076b2f2
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-05-05 21:19:40 +0300

    Update aliaksandr_lahutsin_l5.rb

--- homeworks/aliaksandr_lahutsin_l5.rb
+++ homeworks/aliaksandr_lahutsin_l5.rb
@@ -1,27 +1,21 @@
 # class SuperScript
 class SuperScript < Script
-  def self.write(path, line)
-    if path.nil?
-      puts line
-    else
-      File.open(path, 'w') { |file| file.puts line }
+    def self.write(path, line)
+      if path.nil?
+        puts line
+      else
+        File.open(path, 'w') { |file| file.puts line }
+      end
     end
-  end
   
-  def self.out(**args)
-    if args[:ex].nil?
-      "#{Time.now} #{args[:name]} #{args[:result]}"
+    def self.run(**args)
+      super()
+      block_given? ? result = yield : ''
+    rescue StandardError => ex
+      str = "ERROR: #{Time.now} #{args[:name]} #{ex}"
+      SuperScript.write(args[:stderr_log], str)
     else
-      "ERROR: #{Time.now} #{args[:name]} #{args[:ex]}"
+      str = "#{Time.now} #{args[:name]} #{result}"
+      SuperScript.write(args[:stdout_log], str)
     end
   end
-
-  def self.run(**args)
-    super()
-    block_given? ? result = yield : ''
-  rescue StandardError => ex
-    write(args[:stderr_log], out(args[:name], ex))
-  else
-    write(args[:stdout_log], out(args[:name], result))
-  end
-end
