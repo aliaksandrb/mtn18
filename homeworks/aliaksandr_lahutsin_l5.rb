afefbca03c9f43d6097e8c5de7fb69f9a426da6e
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-05-05 21:24:27 +0300

    Update aliaksandr_lahutsin_l5.rb

--- homeworks/aliaksandr_lahutsin_l5.rb
+++ homeworks/aliaksandr_lahutsin_l5.rb
@@ -1,6 +1,5 @@
-# class SuperScript
 class SuperScript < Script
-    def self.write(path, line)
+  def self.write(path, line)
       if path.nil?
         puts line
       else
@@ -8,14 +7,14 @@ class SuperScript < Script
       end
     end
   
-    def self.run(**args)
-      super()
-      block_given? ? result = yield : ''
-    rescue StandardError => ex
-      str = "ERROR: #{Time.now} #{args[:name]} #{ex}"
-      SuperScript.write(args[:stderr_log], str)
-    else
-      str = "#{Time.now} #{args[:name]} #{result}"
-      SuperScript.write(args[:stdout_log], str)
-    end
-  end
+  def self.run(**args)
+    super()
+    block_given? ? result = yield : ''
+  rescue StandardError => ex
+    str = "ERROR: #{Time.now} #{args[:name]} #{ex}"
+    SuperScript.write(args[:stderr_log], str)
+  else
+    str = "#{Time.now} #{args[:name]} #{result}"
+    SuperScript.write(args[:stdout_log], str)
+  end  
+end
